f98e1e12787ef6bf7fb8bccdac24c9a8bc5fd4d2
citronella3alain <excursion.allen@gmail.com>
2017-12-31 22:28:54 -0800

    New Years Eve Final Version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # TranslateSRT
-This program takes a SRT subtitle file and outputs the SRT file translated.
+Have you a film that you always wanted to watch but couldn't because it was in a foreign language?
+Fear not, TranslateSRT to the rescue! TranslateSRT is a subtitle translator that uses the [DeepL](https://www.deepl.com/)
+translation service to perform its translations.
+
+## Prerequisites
+* [Python 3.6](https://www.python.org/) : Serpentine, but not pearly
+* [pydeepl](https://github.com/EmilioK97/pydeepl) : a Python API wrapper for DeepL translation services
+* [pysrt](https://pypi.python.org/pypi/pysrt) : a Python library to edit and create SubRip files
+## Usage
+`python3 main.py [-h] [-d [IN_LANG]] [infile] [outfile] [translateto]`
+
+
+Example:
+
+
+`python3 main.py -d EN sample.srt out.srt FR`
+
+
+You can find both `sample.srt` and `out.srt` in this repo.
+
+## License
+This project is licensed under the [MIT License](LICENSE).
--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
 # main.py
 import argparse
-import sys
-import pysrt
 import re
+
 import pydeepl
+import pysrt
 
 # Argument Parsing
-parser = argparse.ArgumentParser(description='Translate an SRT file')
-parser.add_argument('infile', nargs='?')
-parser.add_argument('-o', '--outfile', nargs='?')
-parser.add_argument('-d', '--in_lang', nargs='?')
-parser.add_argument('translateto', nargs='?')
+description = """
+Translate an SRT file from and to the following languages:
+English, French, German, Spanish, Italian, Dutch, Polish
+"""
+epilogue = """
+Input and Output modes: 
+auto\t Auto Detect (only for input)\n
+DE\t German\n
+EN\t English\n
+FR\t French\n
+ES\t Spanish\n
+IT\t Italian\n
+NL\t Dutch\n
+PL\t Polish\n
+"""
+parser = argparse.ArgumentParser(description=description, epilog=epilogue)
+parser.add_argument('infile', nargs='?', help = "input file: in.srt")
+parser.add_argument('outfile', nargs='?', help = "output file: out.srt")
+parser.add_argument('-d', '--in_lang', nargs='?', help = "Input Language")
+parser.add_argument('translateto', nargs='?', help = "Output Language")
 args = vars(parser.parse_args())
 
 # Subtitle reading
@@ -19,35 +34,46 @@ lines = pysrt.open(args['infile'])
 # for i in lines:
 #     i.text = re.sub("<[^>]*>", '', i.text)
 #     print(i.text)
-
 index = 0
 sentence = ""
 begin_index = 0
-punctuation = [".","!","?"]
 while index < len(lines):
 
     words = re.findall(r"[\w']+|[.!?]", re.sub("<[^>]*>", '', lines[index].text))
-    #print(words)
+    lines[index].text = ""
     # find last occurrence of a punctuation mark in the words
     index_last_punctuation = -1
     for i in reversed(list(range(len(words)))):
-        if (re.match("[.!?]", words[i])):
+        if re.match("[.!?]", words[i]):
             index_last_punctuation = i
             break
-    #print(index_last_punctuation)
     # if there is no punctuation in this sentence, then add the complete subtitle line to string sentence
     if (index_last_punctuation == -1):
         sentence += " ".join(words)
     else:
-        sentence += (" ".join(words[0:index_last_punctuation+1]) + " ")
-        print(sentence)
-        translated_sentence = pydeepl.translate(sentence, args['translateto'])
-        print(translated_sentence)
+        sentence += (" ".join(words[0:index_last_punctuation + 1]) + " ")
+        if (args["in_lang"] == None):
+            from_lang = "auto"
+        else:
+            from_lang = args["in_lang"]
+        translated_sentence = pydeepl.translate(sentence, args['translateto'], from_lang=from_lang)
+        # Insert translated sentences into the subtitle file.
+        list_trans_sents = translated_sentence.split()  # translated sentences in list form
+        list_trans_sents.insert(int(len(list_trans_sents)/2), "\n")
+        width = int(1 + len(list_trans_sents) / (index - begin_index + 1))  # number of words per line
+        for i in list(range(index - begin_index + 1)):
+            phrase = " ".join(list_trans_sents[i * width:width * (i + 1)])
+            print(phrase)
+            lines[index].text += phrase + "\n"
         # If the sentence ends with a punctuation mark, then the sentence string should be empty
         # Else the sentence string should contain the text up to the end of the string
-        if (index_last_punctuation + 1 == len(words)):
+
+        if index_last_punctuation + 1 == len(words):
             sentence = ""
+            begin_index = index + 1
         else:
             sentence = (" ".join(words[index_last_punctuation + 1:len(words)]) + " ")
+            begin_index = index
 
     index += 1
+lines.save(args['outfile'])
